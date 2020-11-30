7516318225bc0390481be7068ab1750a586ffd94
fagner <fagner_nery@hotmail.com>
2020-11-29 23:28:50 -0300

    Layout atendente

--- menuAtendenteCadInter.php
+++ menuAtendenteCadInter.php
@@ -3,16 +3,16 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Menu Dropdown Responsivo</title>
-        <link rel="stylesheet" href="css/styleMenu.css">
-        <link rel="stylesheet" type="text/css" href="css/estiloCadastro.css">
+		<title>Cadastro Atentende</title>
+        <link rel="stylesheet"  href="css/styleMenu.css">
+        <link rel="stylesheet"  href="css/estiloCadastro.css">
 	</head>
 	<body>
 		<input type="checkbox" id="bt_menu">
 		<label for="bt_menu">&#9776;</label>
 		<nav class="menu">
 			<ul>
-				<li><a href="#">Home</a></li>
+				<li><a href="menuAtendente.php">Home</a></li>
 				<li><a href="#">Internação</a>
 					<ul>
 						<li><a href="menuAtendenteCadInter.php">Cadastrar</a></li>
@@ -25,15 +25,23 @@
 		</nav>
 
        <br><br>
-<div class="container">  
-  <form action="#" class="form-contact" method="post" tabindex="1">  
-     <input type="nome" class="form-contact-input" name="nome" placeholder="Nome Paciente" required />  
-     <input type="medico" class="form-contact-input" name="medico" placeholder="Médico" required />  
-     <input type="plano" class="form-contact-input" name="plano" placeholder="Plano de Saúde" /> 
-     <input type="data" class="form-contact-input" name="data" placeholder="Data Internação" /> 
-     <textarea class="form-contact-textarea" name="conteudo" placeholder="Deixe uma mensagem" required></textarea>  
-     <button type="submit" class="form-contact-button">Enviar</button>  
-  </form>  
+<div class="container" align= "center" >  
+
+  <form action="#" class="form-contact" method="post" tabindex="1" >
+  <br><br> 
+	  <h2 > Cadastro Internação</h2>
+	   <br>  
+     <input  type="nome" class="form-contact-input" name="nome" placeholder="Nome Paciente" required />  
+	 <input type="hospital" class="form-contact-input" name="hospital" placeholder="Hospital" required />
+	 <select  class="form-contact-input" id="procedimento" name="procedimento">
+          <option value="internacao">Internação</option>
+          <option value="outro">Outro</option>
+	</select>  
+	<br> 
+	 <button  type="submit" class="form-contact-button">Enviar</button>
+	 <br>  
+  </form>
+    
 </div> 
 	</body>
 </html>
